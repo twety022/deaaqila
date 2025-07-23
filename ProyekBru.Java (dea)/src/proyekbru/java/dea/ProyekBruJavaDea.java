e84f31324e0bfa1cc8f70b2593405af9b129b35d
twety022 <deaaqilasoleha@gmail.com>
2025-07-23 14:37:30 +0700

    Add files via upload

---
+++ ProyekBru.Java (dea)/src/proyekbru/java/dea/ProyekBruJavaDea.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyekbru.java.dea;
+
+/**
+ *
+ * @author PC 27 - LAB R1
+ */
+public class ProyekBruJavaDea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
